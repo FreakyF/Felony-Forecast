f869ef55f573c63543aa3786d058d0ae8b518894
Kamil Fudala <kamil.fudala44@gmail.com>
2024-10-14 18:55:47 +0200

    Create DataSection component.

--- Frontend/Felony-Forecast/app/index.tsx
+++ Frontend/Felony-Forecast/app/index.tsx
@@ -1,16 +1,24 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import AppearanceOverview from "@/components/appearance-overview/AppearanceOverview";
 import TopAppBar from "@/components/top-app-bar/TopAppBar";
 import BottomAppBar from "@/components/bottom-app-bar/BottomAppBar";
 import HorizontalDivider from "@/components/horizontal-divider/HorizontalDivider";
+import DataSection from "@/components/data-section/DataSection";
 
 export default function Index() {
     return (
-        <View>
-            <TopAppBar title={"Felony Forecast"}></TopAppBar>
-            <AppearanceOverview></AppearanceOverview>
-            <HorizontalDivider subText={"Crime Score"}></HorizontalDivider>
-            <BottomAppBar></BottomAppBar>
+        <View style={styles.mainContainer}>
+            <TopAppBar title={"Felony Forecast"}/>
+            <AppearanceOverview/>
+            <HorizontalDivider subText={"Crime Score"}/>
+            <DataSection/>
+            <BottomAppBar/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1
+    },
+});
---
+++ Frontend/Felony-Forecast/components/data-section/DataSection.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export default function DataSection() {
+    return (
+        <View style={styles.mainContainer}>
+            <Text>No data available.</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        flexShrink: 0,
+        width: 412,
+        height: 53,
+        gap: 4,
+        paddingHorizontal: 15.5,
+        backgroundColor: '#FEF7FF'
+    },
+})
