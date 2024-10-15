79cf7378bc49532f57f68897615dadb67426d14e
Kamil Fudala <kamil.fudala44@gmail.com>
2024-10-15 22:04:03 +0200

    Add picture selection logic.

--- Frontend/Felony-Forecast/components/profile-picture/ProfilePicture.tsx
+++ Frontend/Felony-Forecast/components/profile-picture/ProfilePicture.tsx
@@ -1,10 +1,44 @@
-import {Image, StyleSheet, View} from "react-native";
+import React, {useState} from "react";
+import {Alert, Image, StyleSheet, TouchableOpacity} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
 export default function ProfilePicture() {
+    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState<boolean | null>(null);
+
+    const requestMediaLibraryPermission = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setHasMediaLibraryPermission(permissionResult.granted);
+        return permissionResult.granted;
+    };
+
+    const pickImage = async () => {
+        if (hasMediaLibraryPermission === null) {
+            const granted = await requestMediaLibraryPermission();
+            if (!granted) {
+                Alert.alert("Permission to access the media library is required!");
+                return;
+            }
+        } else if (!hasMediaLibraryPermission) {
+            const granted = await requestMediaLibraryPermission();
+            if (!granted) {
+                Alert.alert("Permission to access the media library is required!");
+                return;
+            }
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync();
+
+        if (result.canceled) {
+            // Handle cancellation logic.
+        } else {
+            // Handle successful photo selected logic.
+        }
+    };
+
     return (
-        <View style={styles.mainContainer}>
-            <Image style={styles.image} source={require("../../assets/images/Image.png")}></Image>
-        </View>
+        <TouchableOpacity onPress={pickImage} style={styles.mainContainer}>
+            <Image style={styles.image} source={require("../../assets/images/Image.png")}/>
+        </TouchableOpacity>
     );
 }
 
@@ -16,5 +50,5 @@ const styles = StyleSheet.create({
         flexShrink: 0,
         width: 150,
         height: 150,
-    }
-})
+    },
+});
