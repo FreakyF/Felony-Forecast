65497731141ec693f8f9c03f1ae1fbb41db4adfa
Kamil Fudala <kamil.fudala44@gmail.com>
2024-10-19 21:48:06 +0200

    Move files and remove unused methods and fix typos.

--- Backend/Felony-Forecast/main.py
+++ Backend/Felony-Forecast/main.py
@@ -8,9 +8,6 @@ from PIL import Image
 app = FastAPI()
 
 list = mock.test.analyzeImage()
-def setPath():
-    for image in list:
-        image.image = image.toBase64(image.image)
 def name_generator():
     now = datetime.datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")
@@ -21,13 +18,8 @@ def Base64ToImage(code):
     image = Image.open(io.BytesIO(imgData))
     image.save(name_generator(),'JPEG')
 
-@app.get("/read_data")
-def read_data():
-    setPath()
-    return (list)
-
-@app.post("/recive_data")
+@app.post("/receive_data")
 async def recive_data(request: Request):
     data = await request.json()
-    print(data['image'])
     Base64ToImage(data['image'])
+    return (list)
